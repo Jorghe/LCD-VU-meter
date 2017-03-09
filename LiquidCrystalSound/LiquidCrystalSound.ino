ddec742d56f591a143f3ffa2ed411f76dbda6c2d
Jorghe Ayala <x.jorghe.x@gmail.com>
2017-03-08 19:17:58 -0600

    Modify directory file

--- LiquidCrystalSound/LiquidCrystalSound.ino
+++ LiquidCrystalSound/LiquidCrystalSound.ino
@@ -1,45 +1,66 @@
 // LiquidCristal Display used as VU meter or SVI, see Readme section for details.
-// 1 audio analog input
-
-// LCD connection are made as Arduino is using: https://www.arduino.cc/en/Tutorial/HelloWorld
-// Full project can be found in:
-// https://github.com/Jorghe/LCD-VU-meter/
+ // 1 audio analog input
+ // LCD connection are made as Arduino is using: https://www.arduino.cc/en/Tutorial/HelloWorld
+ // https://github.com/Jorghe/LCD-VU-meter/
 
 #include  <LiquidCrystal.h>
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
-// Define input sonido as Analog
-long salida;
-int sonido=A0;
+  // Since the size of the matrix might vary, it can be a LCD of 20x4 or 16x2 and so forth
+byte const columns = 20, rows = 4;
+byte arreglo[columns];
+volatile int randomizer;
 
 
 void setup() 
 {
-  // Initialize lcd col and row
-  lcd.begin(20,4);
+  lcd.begin(columns, rows);
 }
 
 void loop() 
 {
-  // Map every analog sound input and scale it to 16 segments, from 0 to 15
-  salida=map(analogRead(sonido),0,700,0,15);
 
-  // Call function from the previous mapped value 
-  vumetro(salida);
+randomizer = generador();
+
+arreglo[0] = randomizer;
+
+printing();
+
+    // This loops works as a method to move every element of the array to the right
+    // it is important to note that these values will be assigned from right to left
+    for (int i=(sizeof(arreglo) - 1); i>0; i--)
+    {
+      arreglo[i] = arreglo[i-1];
+    }
+
+  delay(100);
+
+  lcd.setCursor(0,3);
+  lcd.write((generador(),HEX));
   
-  // For Testing purposes, the following command is used to generate a random value
-  // vumetro(random(0,15));
+} // End of loop
 
-  // Repeat this function every 500 miliseconds
-  delay(500);
+void printing()
+{
+  for (int i=0; i < 20; i++)
+  {
+    vumetro(arreglo[i],i);
+  }
+} 
+
+// This takes an escalated input signal from 0 to 15
+int generador()
+{
+int  entrada = map(analogRead(A0),0,700,0,15);
+
+  return entrada;
 }
 
 
 
-
-void vumetro(int salida)
+int vumetro(int character, int col)
 {
-  // Creating every character according to the corresponding byte
+ // Creating every character according to the corresponding byte
 byte clearChar[8]= {0b00000,0b00000,0b00000,0b00000,0b00000,0b00000,0b00000,0b00000};
 byte  Char0[8] = {0b00000,0b00000,0b00000,0b00000,0b00000,0b00000,0b00000,0b11111};
 byte  Char1[8] = {0b00000,0b00000,0b00000,0b00000,0b00000,0b00000,0b11111,0b11111};
@@ -49,7 +70,6 @@ byte  Char4[8] = {0b00000,0b00000,0b00000,0b11111,0b11111,0b11111,0b11111,0b1111
 byte  Char5[8] = {0b00000,0b00000,0b11111,0b11111,0b11111,0b11111,0b11111,0b11111};
 byte  Char6[8] = {0b00000,0b11111,0b11111,0b11111,0b11111,0b11111,0b11111,0b11111};
 byte  Char7[8] = {0b11111,0b11111,0b11111,0b11111,0b11111,0b11111,0b11111,0b11111};
-
 
   // Load character to the LCD
  lcd.createChar(0, Char0);
@@ -61,145 +81,124 @@ byte  Char7[8] = {0b11111,0b11111,0b11111,0b11111,0b11111,0b11111,0b11111,0b1111
  lcd.createChar(6, Char6);
  lcd.createChar(7, Char7);
  lcd.createChar(8, clearChar);
-  
-  // This tests the mapped value of salida and according to that value, the output will be 
-   switch ( salida )  
-   {
-   case 0 : 
-   {
-     lcd.setCursor(4,0);
-     lcd.write(8);
-     lcd.setCursor(4,1);
-     lcd.write((byte)0);
-  }  
-  break;
-  case 1: 
+
+int salida;
+
+if(salida==0)
   {
-     lcd.setCursor(4,0);
+     lcd.setCursor(col,0);
      lcd.write(8);
-     lcd.setCursor(4,1);
+     lcd.setCursor(col,1);
+     lcd.write((byte)0);
+  }
+else if(salida==1)
+   {
+     lcd.setCursor(col,0);
+     lcd.write(8);
+     lcd.setCursor(col,1);
      lcd.write(1);
    }
-   break;
-   case 2: 
+else if(salida==2)
    {
-     lcd.setCursor(4,0);
+     lcd.setCursor(col,0);
      lcd.write(8);
-     lcd.setCursor(4,1);
+     lcd.setCursor(col,1);
      lcd.write(2);
    }
-   break;
-   case 3: 
+else if(salida==3)
    {
-     lcd.setCursor(4,0);
+     lcd.setCursor(col,0);
      lcd.write(8);
-     lcd.setCursor(4,1);
+     lcd.setCursor(col,1);
      lcd.write(3);
    }
-   break;
-   case 4:
+else if(salida==4)
    {
-     lcd.setCursor(4,0);
+     lcd.setCursor(col,0);
      lcd.write(8);
-     lcd.setCursor(4,1);
+     lcd.setCursor(col,1);
      lcd.write(4);
    }
-   break;
-   case 5:
+else if(salida==5)
    {
-     lcd.setCursor(4,0);
+     lcd.setCursor(col,0);
      lcd.write(8);
-     lcd.setCursor(4,1);
+     lcd.setCursor(col,1);
      lcd.write(5);
    }
-   break;
-   case 6:
+else if(salida==6)
    {
-    lcd.setCursor(4,0);
+    lcd.setCursor(col,0);
      lcd.write(8);
-     lcd.setCursor(4,1);
+     lcd.setCursor(col,1);
      lcd.write(6);
    }
-   break;
-   case 7:
+else if(salida==7)
    {
-    lcd.setCursor(4,0);
+    lcd.setCursor(col,0);
      lcd.write(char(8));
-     lcd.setCursor(4,1);
+     lcd.setCursor(col,1);
      lcd.write(7);
    }
-   break;
-   case 8:
-  {
-    lcd.setCursor(4,0);
+else if(salida==8)
+   {
+    lcd.setCursor(col,0);
      lcd.write((byte)0);
-     lcd.setCursor(4,1);
+     lcd.setCursor(col,1);
      lcd.write(8);
    }
-   break;
-   case 9:
+else if(salida==9)
    {
-    lcd.setCursor(4,0);
+    lcd.setCursor(col,0);
      lcd.write(1);
-     lcd.setCursor(4,1);
+     lcd.setCursor(col,1);
      lcd.write(8);
    }
-   break;
-   case 10:
+else if(salida==10)
    {
-    lcd.setCursor(4,0);
+    lcd.setCursor(col,0);
      lcd.write(2);
-     lcd.setCursor(4,1);
+     lcd.setCursor(col,1);
      lcd.write(8);
    }
-   break;
-   case 11:
+else if(salida==11)
    {
-    lcd.setCursor(4,0);
+    lcd.setCursor(col,0);
      lcd.write(3);
-     lcd.setCursor(4,1);
+     lcd.setCursor(col,1);
      lcd.write(8);
    }
-   break;
-   case 12:
+else if(salida==12)
    {
-    lcd.setCursor(4,0);
+    lcd.setCursor(col,0);
      lcd.write(4);
-     lcd.setCursor(4,1);
+     lcd.setCursor(col,1);
      lcd.write(8);
    }
-   break;
-   case 13:
+else if(salida==13)
    {
-    lcd.setCursor(4,0);
+    lcd.setCursor(col,0);
      lcd.write(5);
-     lcd.setCursor(4,1);
+     lcd.setCursor(col,1);
      lcd.write(8);
    }
-   break;
-   case 14:
+else if(salida==14)
    {
-    lcd.setCursor(4,0);
+    lcd.setCursor(col,0);
      lcd.write(6);
-     lcd.setCursor(4,1);
+     lcd.setCursor(col,1);
      lcd.write(8);
    }
-   break;
-   case 15:
+else if(salida==15)
    {
-     lcd.setCursor(4,0);
+     lcd.setCursor(col,0);
      lcd.write(7);
-     lcd.setCursor(4,1);
+     lcd.setCursor(col  ,1);
      lcd.write(8);
    }
-   break;
-   default:
-   {
-     lcd.setCursor(4,0);
-     lcd.write(byte(0));
-     lcd.setCursor(4,1);
-     lcd.write(byte(0));
-    }
-    break;
-   }
+ 
+ /*
+   lcd.setCursor(col,3);
+   lcd.print(salida, HEX);
+*/
 }
